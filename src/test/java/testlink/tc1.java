91627fe2a3b958b1a956c2bc0247448e2c1ba365
AlwaysTerry <admin123>
2014-09-01 13:20:47 +0800

    d

---
+++ src/test/java/testlink/tc1.java
@@ -0,0 +1,13 @@
+package testlink;
+
+import org.testng.annotations.Test;
+
+import junit.framework.Assert;
+
+public class tc1 {
+
+	@Test
+	public void test1(){
+		Assert.assertEquals(true, true);
+	}
+}
